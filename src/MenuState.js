67b7719f334db53093a31b607f3b244884d05d82
iver56 <iver56@hotmail.com>
2019-11-15 22:23:44 +0100

    Remove some unused code. Start the music once the game starts.

--- src/MenuState.js
+++ src/MenuState.js
@@ -4,57 +4,24 @@ function MenuState() {
 MenuState.prototype.init = function() {
   this.bg_img = loadImage('res/menu.png');
 
-  this.key_cooldown = 0;
-
   var that = this;
   this.elements = [
     [function() {
-      sm.changeState('game');
-      mm.changeState('game');
+      that.startGame();
     }, {x: 13.35, y: 3.9, w: 1.9, h: 1.2, hover: function() {
-      that.select(0);
     }}],
     [function() {
       mm.audioButton.toggleActivated();
     }, {x: 15, y: 0, w: 1, h: 1}]
   ];
 
-  this.y_values = [1.8, 3.3, 4.8];
-  this.y_time = 1;
-  this.y = this.y_values[0];
-  this.start_y = this.y_values[0];
-  this.end_y = this.y_values[0];
   this.selected = 0;
 };
 
-MenuState.prototype.setY = function(y) {
-  this.start_y = this.y;
-  this.end_y = y;
-  this.y_time = 0;
-};
-
-MenuState.prototype.select = function(selected) {
-  if (this.selected == selected) {
-    return;
-  }
-  this.selected = selected;
-  this.setY(this.y_values[this.selected]);
-};
-
 MenuState.prototype.pause = function() {
-  document.removeEventListener('keypress', this.fullscreenHandler);
-  $("#wrapper > .logo").remove();
-  $('.player-name-container').hide();
 };
 
 MenuState.prototype.resume = function() {
-  var that = this;
-  this.fullscreenHandler = document.addEventListener('keypress', function(e) {
-    if (e.keyCode == 13 && that.selected == 0) {
-    }
-  });
-  var logo = $('.logo.template').clone().removeClass('template');
-  $('#wrapper').append(logo);
 };
 
 MenuState.prototype.render = function(ctx) {
@@ -69,30 +36,18 @@ MenuState.prototype.render = function(ctx) {
   mm.audioButton.render(ctx);
 };
 
-MenuState.prototype.update = function() {
-  this.key_cooldown && this.key_cooldown--;
-  this.y_time += 0.12;
-  if (this.y_time < 1) {
-    this.y = smoothstep(this.start_y, this.end_y, this.y_time);
-  } else if (this.y_time > 1) {
-    this.y_time = 1;
-    this.y = this.end_y;
+MenuState.prototype.startGame = function() {
+  if (mm.loaded) {
+    sm.changeState('game');
+    mm.changeState('game');
+  } else {
+    alert('The music has not finished loading yet. Try again soon')
   }
 
+};
 
-  if (!this.key_cooldown) {
-    if (KEYS[40]) { /* key down */
-      this.selected + 1 < this.y_values.length && this.select(this.selected + 1);
-      this.key_cooldown = 10;
-    }
-    if (KEYS[38]) { /* key up */
-      this.selected - 1 >= 0 && this.select(this.selected - 1);
-      this.key_cooldown = 10;
-    }
-    if (KEYS[13]) { /* key enter */
-      this.key_cooldown = 10;
-      sm.changeState('game');
-      mm.changeState('game');
-    }
+MenuState.prototype.update = function() {
+  if (KEYS[13]) { /* key enter */
+    this.startGame();
   }
 };
--- src/MusicManager.js
+++ src/MusicManager.js
@@ -2,25 +2,21 @@ function MusicManager() {
   this.audioButton = new AudioButton();
   this.music = new Audio();
   this.loaded = false;
+  this.tempo = 96;  // in bpm
   var that = this;
   loaded++;
   this.music.addEventListener("loadeddata", function() {
     that.loaded || loaded--;
     that.loaded = true;
-    this.play()
   });
   this.music.addEventListener("canplay", function() {
     that.loaded || loaded--;
     that.loaded = true;
-    this.play()
   });
-  this.music.volume = 0.4;
+  this.music.volume = 0.6;
   this.music.src = "res/music.ogg";
   this.state = "menu";
   this.musictimes = {
-    menustart: 0,
-    menuend: 70,
-    menulength: 70,
     gamestart: 0,
     gameend: 70,
     gamelength: 70
@@ -30,17 +26,17 @@ function MusicManager() {
 
 MusicManager.prototype.changeState = function(state) {
   this.state = state;
+  if (state === 'game') {
+    this.music.play();
+  }
 };
 
 MusicManager.prototype.update = function() {
-  if (this.loaded) {
-    if (this.state == "menu" && this.music.currentTime > this.musictimes.menuend) {
-      this.music.currentTime -= this.musictimes.menulength;
-    } else if (this.state == "game" && this.music.currentTime < this.musictimes.gamestart) {
+  if (this.loaded && this.state === "game") {
+    if (this.music.currentTime < this.musictimes.gamestart) {
       this.music.currentTime = this.musictimes.gamestart;
     } else if (this.music.currentTime > this.musictimes.gameend) {
       this.music.currentTime -= this.musictimes.gamelength;
     }
   }
 };
-
